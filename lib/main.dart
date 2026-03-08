5779a5ca48c8feb6756c6c4bf36e819ccaa01e83
rajamransri-blip <rajamransri@gmail.com>
2026-03-08 21:32:38 +0530

    Create lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Gamer Pro Tool',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark),
+      home: const HomePage(),
+    );
+  }
+}
